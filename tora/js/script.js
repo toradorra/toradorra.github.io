390816c121ed7e88c95f4c438360ad45798eebf8
toradorra <victoriapisareva16@gmail.com>
2019-05-27 21:44:16 +0300

    tora

--- tora/js/script.js
+++ tora/js/script.js
@@ -52,3 +52,10 @@ function triggerStats(){
     });
   });
 };
+
+const instance = new TypeIt('#replaceStrings', {
+ strings: [". Добро пожаловать", ". Привет"],
+ speed: 200,
+ breakLines: true,
+ waitUntilVisible: true
+}).go();
