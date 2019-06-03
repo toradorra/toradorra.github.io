271c6a972b0513b9a55c51595b28926641e03d59
toradorra <victoriapisareva16@gmail.com>
2019-06-03 14:38:37 +0300

    tora

--- tora/js/script.js
+++ tora/js/script.js
@@ -52,3 +52,11 @@ function triggerStats(){
     });
   });
 };
+
+$(document).ready(function () {
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true
+  })
+});
