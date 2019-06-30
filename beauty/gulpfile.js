ae31be4d57f9a5ef970e23fc157ec62fdf536b29
aesfur <aesfur@gmail.com>
2019-07-01 00:08:01 +0300

    Fixes beauty

--- beauty/gulpfile.js
+++ beauty/gulpfile.js
@@ -1,25 +1,87 @@
-var gulp      = require('gulp'), // Подключаем Gulp
-    sass        = require('gulp-sass'), //Подключаем Sass пакет,
-    browserSync = require('browser-sync'); // Подключаем Browser Sync
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const del = require("del");
+const browsersync = require("browser-sync").create();
 
-gulp.task('sass', function(){ // Создаем таск Sass
-    return gulp.src('app/sass/**/*.sass') // Берем источник
-        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
-        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
-});
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: ".",
+    },
+    port: 3000,
+  });
+  done();
+}
 
-gulp.task('browser-sync', function() { // Создаем таск browser-sync
-    browserSync({ // Выполняем browserSync
-        server: { // Определяем параметры сервера
-            baseDir: 'app' // Директория для сервера - app
-        },
-        notify: false // Отключаем уведомления
-    });
-});
 
-gulp.task('watch', function() {
-    gulp.watch('app/sass/**/*.sass', gulp.parallel('sass')); // Наблюдение за sass файлами
-});
-gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
-    
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+
+function clean() {
+  return del(["dist"]);
+}
+
+
+function images() {
+  return gulp
+    .src("app/img/**/*")
+    .pipe(gulp.dest("dist/img"));
+}
+
+
+function videos() {
+  return gulp
+    .src("app/video/**/*")
+    .pipe(gulp.dest("dist/video"));
+}
+
+
+function css() {
+  return gulp
+    .src(["app/sass/**/*.sass", "app/css/**/*.css"])
+    .pipe(sass({outputStyle: "expanded"}))
+    .pipe(gulp.dest("dist/css/"))
+    .pipe(browsersync.stream());
+}
+
+
+function scripts() {
+  return (
+    gulp
+      .src(["app/js/**/*"])
+      .pipe(gulp.dest("dist/js/"))
+      .pipe(browsersync.stream())
+  );
+}
+
+
+function watchFiles() {
+  gulp.watch(["app/sass/**/*", "app/css/**/*.css"], css);
+  gulp.watch("app/js/**/*");
+  gulp.watch(
+    [
+      "./_includes/**/*",
+      "./_layouts/**/*",
+      "./_pages/**/*",
+      "./_posts/**/*",
+      "./_projects/**/*",
+    ],
+    gulp.series(browserSyncReload),
+  );
+  gulp.watch(".app/img/**/*", images);
+}
+
+const js = gulp.series(scripts);
+const build = gulp.series(clean, gulp.parallel(css, images, videos, js));
+const watch = gulp.parallel(watchFiles, browserSync);
+
+exports.images = images;
+exports.css = css;
+exports.js = js;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
