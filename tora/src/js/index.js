a5172a70052dc007021b81ca2de3e924ff74cd86
toradorra <victoriapisareva16@gmail.com>
2019-07-11 12:00:38 +0300

    fix mobile media

--- tora/src/js/index.js
+++ tora/src/js/index.js
@@ -1,3 +1,82 @@
+$("#toggle").click(function() {
+  $("#burger-menu").toggleClass("open");
+  $("#toggle").toggleClass("open");
+  $("#menu").toggleClass("open");
+});
+
+$(function() {
+  $(".burger-box,.sliding-panel-fade-screen,.sliding-panel-close").on(
+    "click touchstart",
+    function(e) {
+      e.preventDefault();
+      $(".burger-box").toggleClass("is-open");
+      $(".sliding-panel-content,.sliding-panel-fade-screen").toggleClass(
+        "is-visible"
+      );
+    }
+  );
+});
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
+
+
+
 var tooltiptext;
   $(".tooltip").mouseenter(function () {
     var thisStat = $(this);
@@ -43,6 +122,7 @@ function triggerStats() {
     });
   });
 };
+
 
 $(document).ready(function () {
   var mySwiper = new Swiper('.swiper-container', {
